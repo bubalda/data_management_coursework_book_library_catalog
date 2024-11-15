576fcd72321e4abb2ea9861f56878e860e407369
bubalda <hcm1e24@soton.ac.uk>
2024-11-15 21:32:54 +0800

    fu

--- AnalyticalQuery.php
+++ AnalyticalQuery.php
@@ -14,7 +14,7 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Analytical Queries</h1>
-                    <p class="mb-4">All Analytical Queries and Their results will be shown here From WEEK-5 Lab Handout.
+                    <p class="mb-4">All Analytical Queries and Their results are shown here.
                         </p>
                     
                         <?php
