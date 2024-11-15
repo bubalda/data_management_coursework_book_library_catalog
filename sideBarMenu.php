90b26d2d6ee02d4e4453d59d95d03b83bc487fca
bubalda <hcm1e24@soton.ac.uk>
2024-11-15 21:35:22 +0800

    predictable

--- sideBarMenu.php
+++ sideBarMenu.php
@@ -61,7 +61,7 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
+                        <h6 class="collapse-header">Tables:</h6>
                         <a class="collapse-item" href="UserTable.php">User Table</a>
                         <a class="collapse-item" href="AuthorTable.php">Author Table</a>
                         <a class="collapse-item" href="BookTable.php">Book Table</a>
