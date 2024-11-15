027d6ff1e9d4ddb45ed79d091c39b7a4abefc390
bubalda <hcm1e24@soton.ac.uk>
2024-11-15 20:51:41 +0800

    pushlast

--- connectDB (Dep).php
+++ connectDB (Dep).php
@@ -1,19 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-connectDB.php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// connectDB.php
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+// if (!$conn) {
+  // die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
 
 
-mysqli_close($conn);
+// mysqli_close($conn);
 ?>
 
 
